a523a26bd866a7d54a1c68b1fbae3aeb329244f0
Pritam Ghosh <contact.meghoshpritam@gmail.com>
2024-09-08 03:09:30 +0530

    refactor: Add authentication check in buildTrigger controller

--- src/controller/builderTrigger.controller.ts
+++ src/controller/builderTrigger.controller.ts
@@ -1,8 +1,30 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+
+const checkAuth = (authHeader: string) => {
+  if (!authHeader) {
+    return false;
+  }
+
+  const [bearer, token] = authHeader.split(' ');
+
+  if (bearer !== 'Bearer' || !bcrypt.compareSync(token, process.env.SECRET || '')) {
+    return false;
+  }
+
+  return true;
+};
 
 export const buildTrigger = async (request: FastifyRequest, reply: FastifyReply) => {
   dotenv.config();
-  console.log('📢[builderTrigger.controller.ts:5]: request: ', request.body);
-  return { message: 'Hello from a separate controller file!' };
+
+  const authHeader = request.headers.authorization ?? '';
+
+  if (!checkAuth(authHeader)) {
+    reply.code(401).send({ message: 'Unauthorized' });
+    return;
+  }
+
+  return { message: 'der!' };
 };
