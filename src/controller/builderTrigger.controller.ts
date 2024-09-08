32575bf58228bddecd3e4422ae3675901a403c13
Pritam Ghosh <contact.meghoshpritam@gmail.com>
2024-09-08 11:41:38 +0530

    feat: Add yaml package to package.json and pnpm-lock.yaml

--- src/controller/builderTrigger.controller.ts
+++ src/controller/builderTrigger.controller.ts
@@ -1,6 +1,13 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
+import yaml from 'yaml';
+import fs from 'fs';
+
+interface ConfigTypes {
+  id: string;
+  path: string;
+}
 
 const checkAuth = (authHeader: string) => {
   if (!authHeader) {
@@ -16,13 +23,47 @@ const checkAuth = (authHeader: string) => {
   return true;
 };
 
-export const buildTrigger = async (request: FastifyRequest, reply: FastifyReply) => {
+const getProjectConfigurations = () => {
+  const configurationText = fs.readFileSync('config.yml', 'utf-8');
+
+  const configurations = yaml.parse(configurationText).projects;
+
+  return configurations.reduce(
+    (
+      acc: {
+        [key: string]: ConfigTypes;
+      },
+      config: ConfigTypes,
+    ) => {
+      acc[config.id] = config;
+      return acc;
+    },
+    {},
+  );
+};
+
+export const buildTrigger = async (request: FastifyRequest<{ Body: { project: string } }>, reply: FastifyReply) => {
   dotenv.config();
 
   const authHeader = request.headers.authorization ?? '';
 
   if (!checkAuth(authHeader)) {
     reply.code(401).send({ message: 'Unauthorized' });
+    return;
+  }
+
+  const projectId = request.body.project;
+
+  if (!projectId) {
+    reply.code(400).send({ message: 'Project ID is required' });
+    return;
+  }
+
+  const configurations = getProjectConfigurations();
+  const projectConfig = configurations[projectId];
+
+  if (!projectConfig) {
+    reply.code(404).send({ message: 'Project not found' });
     return;
   }
 
