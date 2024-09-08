c6db70801e36caae9314b7782ea398f6104e62fe
Pritam Ghosh <contact.meghoshpritam@gmail.com>
2024-09-08 13:11:27 +0530

    Refactor build script to optimize package installation

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ git pull origin main
 
 nvm use $(cat .nvmrc)
 
-npx pnpm install --frozen-lockfile --prefer-frozen-lockfile --prod -y
+npx pnpm install --frozen-lockfile --prefer-frozen-lockfile --prod --no-optional --no-audit --no-fund --no-save --no-interactive
 
 npx pnpm run build
